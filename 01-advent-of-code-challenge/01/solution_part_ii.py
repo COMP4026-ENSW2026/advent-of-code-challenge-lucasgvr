6bad0566ac9e4ec613c5a5a1372663c6202025a2
Laboratório Informática <labinfo@unifil.br>
2023-02-08 20:16:26 -0300

    Day 1 - Done

---
+++ 01-advent-of-code-challenge/01/solution_part_ii.py
@@ -0,0 +1,33 @@
+with open('sample.in') as file:
+    data = [i for i in file.read().strip().split("\n")]
+    
+maxCal = 0
+count = 0
+
+for item in data:
+    if item == '':
+        count = 0
+    else:
+        num = int(item)
+        count += num
+        
+    if count > maxCal:
+        maxCal = count
+    
+highest = []
+sums = 0
+
+for item in data:
+    if item == '':
+        highest.append(sums)
+        sums = 0
+    else:
+        sums += int(item)
+    
+finalSum = 0
+
+for i in range(3):
+    finalSum += max(highest)
+    highest.remove(max(highest))
+    
+print(finalSum)
